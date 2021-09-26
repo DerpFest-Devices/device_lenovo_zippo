Lucchetto/android_device_lenovo_zippo

zippo: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_zippo.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zippo-user \
+    lineage_zippo-userdebug \
+    lineage_zippo-eng

DerpFest-Devices/device_lenovo_zippo

AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/derp_zippo.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_zippo-user \
+    derp_zippo-userdebug \
+    derp_zippo-eng